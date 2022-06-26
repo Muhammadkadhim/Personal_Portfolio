89faf5c603a9a6dbb74167eee0ec73f5904d1789
Muhammadkadhim <muhammad.kadhim.hassan@gmail.com>
2022-06-26 20:07:44 +0300

    updating main.js and index.html

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
 // including html files in index.html
-$(function () {
-  var includes = $("[data-include]");
-  $.each(includes, function () {
-    var file = $(this).data("include");
-    $(this).load(file);
-  });
-});
+// $(function () {
+//   var includes = $("[data-include]");
+//   $.each(includes, function () {
+//     var file = $(this).data("include");
+//     $(this).load(file);
+//   });
+// });
+
+// let includes = document.querySelectorAll('[data-include]')
+// includes.forEach(includes, ()=>{
+//   let file =
+// })
+
+class HTMLInclude extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = "Loading...";
+    this.loadContent();
+  }
+
+  async loadContent() {
+    const source = this.getAttribute("src");
+    if (!source) {
+      throw new Error("No src attribute given.");
+    }
+    const response = await fetch(source);
+    if (response.status !== 200) {
+      throw new Error(`Could not load resource: ${source}`);
+    }
+    const content = await response.text();
+    this.innerHTML = content;
+  }
+}
+
+window.customElements.define("html-include", HTMLInclude);
