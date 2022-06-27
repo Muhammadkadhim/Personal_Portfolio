d58fef1d6c9035fa40d569e6018cf83b6acb0f1a
Muhammadkadhim <muhammad.kadhim.hassan@gmail.com>
2022-06-27 16:42:01 +0300

    adding contact functionality

---
+++ js/contact.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+    let TEMPLATE = config.EMAIL_JS_TEMPLATE;
+    let SERVICE = config.EMAIL_JS_SERVICE;
+    document
+        .getElementById("contact_form")
+        .addEventListener("submit", function (event) {
+            event.preventDefault();
+            // generate a five digit number for the contact_number variable
+            this.contact_number.value = (Math.random() * 100000) | 0;
+            // these IDs from the previous steps
+            emailjs.sendForm(`${SERVICE}`, `${TEMPLATE}`, this).then(
+                function () {
+                    alert("Your question was sent successfully!");
+                },
+                function (error) {
+                    console.log("FAILED...", error);
+                    alert("Sorry! an error has occured");
+                }
+            );
+
+            // event.reset();
+        });
+};
---
+++ js/emailjs.js
@@ -0,0 +1,4 @@
+(function () {
+    let API_KEY = config.EMAIL_JS_API_KEY;
+    emailjs.init(`${API_KEY}`);
+})();
